160efac1737429a62870e016dbdba3673a3490fe
DannyBetancourt <danny@babelian.org>
2015-01-27 00:54:39 -0600

    Created by Coding Ground Client

---
+++ OneSmallStepAndAllThat/main.cs
@@ -0,0 +1,11 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("How else does one begin to explore the world, than by first stepping out of one's native land? Hello, World. Here I come.");
+        Console.ReadKey();
+    }
+}
